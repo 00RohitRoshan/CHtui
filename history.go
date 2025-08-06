352af85d092cab47e88999765f1d4c447324a8de
rohit roshan jena <rohit.r@iserveu.co.in>
2025-08-06 17:05:53 +0530

    remove from history function

--- history.go
+++ history.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
 func (h *QueryHistoryManager) Add(query string) {
 	for _, q := range h.history {
 		if q == query {
@@ -12,6 +17,24 @@ func (h *QueryHistoryManager) Add(query string) {
 	}
 }
 
+func (h *QueryHistoryManager) clear(a string) string {
+	i, err := strconv.Atoi(a)
+	if err != nil {
+
+		return fmt.Sprintf("Invalid index:", a)
+	}
+
+	// Convert to 0-based index
+	index := i - 1
+
+	if index < 0 || index >= len(h.history) {
+		return "Index out of range"
+	}
+
+	h.history = append(h.history[:index], h.history[index+1:]...)
+	return ""
+}
+
 func (h *QueryHistoryManager) Navigate(dir int) string {
 	h.index += dir
 	if h.index < 0 {
@@ -19,13 +42,12 @@ func (h *QueryHistoryManager) Navigate(dir int) string {
 	} else if h.index >= len(h.history) {
 		h.index = len(h.history) - 1
 	}
-	if len(h.history) == 0{
+	if len(h.history) == 0 {
 		return ""
-	} 
+	}
 	return h.history[h.index]
 }
 
 func (h *QueryHistoryManager) GetAll() []string {
 	return h.history
 }
-
